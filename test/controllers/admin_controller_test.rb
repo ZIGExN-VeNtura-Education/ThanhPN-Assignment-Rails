7597f9b791385955ee545c3480368bc723fd4c28
RioLeon <138135757+RioLeon@users.noreply.github.com>
2024-05-14 16:06:46 +0700

    Add some basic controllers testing (#2)

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -5,4 +5,70 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  def setup
+    super
+    @admin = users(:admin)
+    @mod = users(:moderator)
+    @guest = users(:guest)
+  end
+
+  test "Redirect when no logged" do
+    get admin_path
+    assert_redirected_to new_user_session_path
+  end
+
+  test "Redirect when logged with non manager account" do
+    get admin_path
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+    post new_user_session_path(
+           params:
+             { user:
+                 {
+                   email: @guest.email,
+                   password: '123456'
+                 }
+             }
+         )
+    assert_redirected_to root_path
+    get admin_path
+    assert_redirected_to root_path
+
+  end
+
+  test "Accept when logged by moderator account" do
+    get admin_path
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+    post new_user_session_path(
+           params:
+             { user:
+                 {
+                   email: @mod.email,
+                   password: '123456'
+                 }
+             }
+         )
+    assert_redirected_to root_path
+    get admin_path
+    assert_equal "show", @controller.action_name
+  end
+
+  test "Accept when logged by admin account" do
+    get admin_path
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+    post new_user_session_path(
+           params:
+             { user:
+                 {
+                   email: @admin.email,
+                   password: '123456'
+                 }
+             }
+         )
+    assert_redirected_to root_path
+    get admin_path
+    assert_equal "show", @controller.action_name
+  end
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -4,4 +4,38 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @book = books(:one)
+    @admin = users(:admin)
+    @mod = users(:moderator)
+    @guest = users(:guest)
+  end
+
+  test "Guest cannot access book info" do
+    get book_path(@book.id)
+    assert_redirected_to new_user_session_path
+  end
+
+  test "logged user can access book info" do
+    get book_path(@book.id)
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+    post new_user_session_path(
+           params:
+             { user:
+                 {
+                   email: @guest.email,
+                   password: '123456'
+                 }
+             }
+         )
+    get book_path(@book.id)
+    assert_response :success
+  end
+
+  test "non moderator user cannot access book create" do
+    get new_book_path(@book.id)
+    assert_redirected_to new_user_session_path
+  end
+
 end
